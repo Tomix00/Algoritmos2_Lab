bda47de657c5b60fa0c1838cbfb574b07a6a5a80
Luciano Tomás Quevedo <104854482+Tomix00@users.noreply.github.com>
2024-04-22 04:37:18 -0300

    Create array_helpers.c

---
+++ Lab01/ej3/array_helpers.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "array_helpers.h"
+
+
+unsigned int array_from_file(int array[],
+                             unsigned int max_size,
+                             const char *filepath) {
+
+    FILE *lista = fopen (filepath, "r");
+    unsigned int longitud;
+    fscanf (lista, "%u", &longitud);
+    assert (longitud<max_size);
+    unsigned int i=0;
+    while (i<longitud) {
+        fscanf (lista, "%d", &array[i]);
+        i=i+1;
+    }
+    fclose (lista);
+
+    return longitud;
+}
+
+void array_dump(int a[], unsigned int length) {
+
+    printf("[ ");
+    for (unsigned int cont = 0; cont<length; cont++) {
+        printf("%d ", a[cont]);
+    } 
+    printf("]\n");
+}
