e34d013afeffcfe53a92d3722a16e759a6d840f5
Luciano Tomás Quevedo <104854482+Tomix00@users.noreply.github.com>
2024-05-17 02:50:09 -0300

    Update strfuncs.h

--- lab05/ej1/a-strfuncs/strfuncs.h
+++ lab05/ej1/a-strfuncs/strfuncs.h
@@ -1,10 +1,11 @@
 #include <stdbool.h>
+#include <stdlib.h>
 
 size_t string_length(const char *str);
-//{Devuelve la el tamaño del arreglo}
+//{devuelve el tamaño del string}
 
-char *string_filter(const char *str, char c);
-//{Devuelve otro string sin el caracter c}
+char * string_filter(const char *str, char c);
+//{devuelve otro string sin el caracter c}
 
 bool string_is_symmetric(const char *str);
-//{comprueba si el string es capicua}
+//{verifica si el string es palindromo}
