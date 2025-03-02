a64919c3cbbc0cb72274a94f3200c4a2587b7922
Luciano Tomás Quevedo <104854482+Tomix00@users.noreply.github.com>
2025-03-01 21:10:19 -0300

    Lab05 added with README

---
+++ Lab05/README.md
@@ -0,0 +1,8 @@
+| Lab05  | Descripcion                                                           |
+|--------|-----------------------------------------------------------------------|
+| Ej1 :  | string operation with pointers and diference of sizeof() y strlen()+1 |
+| Ej2a : | TAD stack                                                             |
+| Ej2b : | TAD stack de file, se agrega Makefile                                 |
+| Ej3 :  | lo mismo                                                              |
+| Ej4 :  | hanoi with TAD stack                                                  |
+| Ej5 :  | TAD queue                                                             |
---
+++ Lab05/ej2a/stack.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <assert.h>
+
+#include "stack.h"
+
+struct _s_stack{
+    stack_elem elem;
+    stack next;
+};
+
+stack stack_empty(){
+    return NULL;
+}
+
+//misma idea que addl
+stack stack_push(stack s , stack_elem e){
+    stack new_stack;
+
+    new_stack = malloc(sizeof(struct _s_stack));    
+    new_stack->elem = e;
+    new_stack->next = s;
+
+    return new_stack;
+}
+
+//misma idea que tail
+stack stack_pop(stack s){
+    assert(!stack_is_empty(s));
+
+    stack ptr_stack = s;
+    ptr_stack = ptr_stack->next;
+    free(s);
+
+    return ptr_stack;
+}
+
+unsigned int stack_size(stack s){
+    stack ptr_stack = s;
+    unsigned int size=0;
+    
+    while(ptr_stack != NULL){
+        ptr_stack = ptr_stack->next;
+        size++;
+    }
+    return size;
+}
+
+stack_elem stack_top(stack s){
+    assert(!stack_is_empty(s));
+
+    stack ptr_stack = s;
+    stack_elem elem = ptr_stack->elem;
+
+    return elem;
+}
+
+bool stack_is_empty(stack s){
+    bool res;
+    res = (s == NULL);
+    return res;
+}
+
+stack_elem *stack_to_array(stack s){
+    unsigned int size = stack_size(s);
+    
+    stack_elem *array=NULL;
+    
+    if(size>0){
+        stack ptr_stack = s;
+        array = calloc(size,sizeof(stack_elem));
+
+        for(int i=size-1;i>=0;i--){
+            array[i] = ptr_stack->elem;
+            ptr_stack = ptr_stack->next;
+        }
+    }
+
+    return array;
+}
+
+stack stack_destroy(stack s){
+    if (s != NULL){
+        stack_destroy(s->next);
+        free(s);
+    }
+    s = NULL;
+
+    return s;
+}
